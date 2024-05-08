7603aa285f42101f246f1c81b718164b761dee29
Bossanogi <blackfire502@gmail.com>
2024-05-09 01:17:27 +0300

    Refactoring and pass test

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,11 @@ func main() {
 	fmt.Println(Hello("word!"))
 }
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, world!"
+	if name == "" {
+		name = "world"
+	}
+	return englishHelloPrefix + name + "!"
 }
