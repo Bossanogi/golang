84bbf7bf91097381b25b8adb044f3d9515f392cf
Bossanogi <blackfire502@gmail.com>
2024-05-10 02:30:41 +0300

    Feature: imlemating func Sub Test: func Add and fucn Sub pass Refactoring: rename adder.go to tools.go

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -3,10 +3,21 @@ package integers
 import "testing"
 
 func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
+	t.Run("testing func Add", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
+		assertCorrectMessage(t, sum, expected)
+	})
+	t.Run("testing func Sub", func(t *testing.T) {
+		substruct := Sub(3, 1)
+		expected := 2
+		assertCorrectMessage(t, substruct, expected)
+	})
+}
 
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
+func assertCorrectMessage(t testing.TB, result, expected int) {
+	t.Helper()
+	if result != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, result)
 	}
 }
---
+++ integers/tools.go
@@ -0,0 +1,11 @@
+package integers
+
+// func Add adding x to y
+func Add(x, y int) int {
+	return x + y
+}
+
+// func Sub substruct y onto x
+func Sub(x, y int) int {
+	return x - y
+}
