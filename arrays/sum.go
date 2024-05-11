8d5f91c536756851b3829211605279b82d177bbe
Bossanogi <blackfire502@gmail.com>
2024-05-12 02:08:23 +0300

    Fix: func SumAllTail change range to delete in slices.Delete Test: func SumAllTail - pass

--- arrays/sum.go
+++ arrays/sum.go
@@ -25,12 +25,12 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 	for _, numbers := range numbersToSum {
 		sums = append(sums, SumSlice(numbers))
 	}
-	return sums
+	return
 }
 
 func SumAllTail(numbersToSum ...[]int) (sums []int) {
 	for _, numbers := range numbersToSum {
-		numbers = slices.Delete(numbers, 0, 0)
+		numbers := slices.Delete(numbers, 0, 1)
 		sums = append(sums, SumSlice(numbers))
 	}
 
