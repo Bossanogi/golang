7b1a00f54a1a41398f979ac32f4168ddd8e16a4f
Bossanogi <blackfire502@gmail.com>
2024-05-13 01:33:00 +0300

    Fix: add empty slice support for func SumAllTail Test: func SumAllTail - pass
    Func SumAllTail now can take  empty slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -37,10 +37,20 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTail(t *testing.T) {
-	got := SumAllTail([]int{1, 2}, []int{0, 9}, []int{2, 3, 1})
-	want := []int{2, 9, 4}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2}, []int{0, 9}, []int{2, 3, 1})
+		want := []int{2, 9, 4}
 
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("try empty slices", func(t *testing.T) {
+		got := SumAllTail([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
